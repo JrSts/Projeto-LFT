dc14a5d43c8ef9d3dd4fc5eddda4a3fe48b00cf8
nova pasta <paulovitordossf@gmail.com>
2020-06-11 15:14:38 -0300

    tabela de simbolo

---
+++ SemanticVisitor.py
@@ -0,0 +1,20 @@
+from AbstractVisitor import AbstractVisitor
+import SymbolTable as st
+from Visitor import Visitor
+from lex import lex
+import ConcretaClass as co
+
+def coercion(type1, type2):
+    if (type1 in st.Number and type2 in st.Number):
+        if (type1 == st.FLOAT or type2 == st.FLOAT):
+            return st.FLOAT
+        else:
+            return st.INT
+    else:
+        return None
+
+class SemanticVisitor(AbstractVisitor):
+
+    def __init__(self):
+        self.printer = Visitor()
+        st.beginScope('main')
--- SymbolTable.py
+++ SymbolTable.py
@@ -5,11 +5,40 @@ BOOL = 'boolean'
 TYPE = 'type'
 PARAMS = 'params'
 BINDABLE = 'bindable'
-FUNCTION = 'fun'
-VARIABLE = 'var'
+FUNCTION = 'function'
+VARIABLE = 'variable'
 SCOPE = 'scope'
+FOR = 'for'
+IF = 'if'
+WHILE ='while'
+VAR = 'var'
+VAL = 'val'
+FUN = 'fun'
+NULL = 'null'
+RETURN = 'return'
+FALSE = 'false'
+TRUE = 'true'
+ELSE = 'else'
+CONTINUE = 'continue'
+DO = 'do'
+IN = 'in'
+IS = 'is'
+BREAK = 'break'
+NOT_IN = 'not_in'
+NOT_IS = 'not_is'
+AS = 'as'
+DOUBLE = 'double'
+LONG ='long'
+STRING = 'string'
+BYTE = 'byte'
+SHORT = 'short'
+PRINT = 'print'
+PRINTLN = 'println'
+SIM = 'sim'
+NAO = 'nao'
+USANDO ='use'
 Number = [INT, FLOAT]
-
+primitivos = [BYTE,SHORT,INT,LONG]
 def beginScope(nameScope):
     global symbolTable
     symbolTable.append({})
@@ -17,7 +46,13 @@ def beginScope(nameScope):
 
 def endScope():
     global symbolTable
+    dictionarylist = []
+    for x in symbolTable[-1]:
+        if(type(symbolTable[-1][x]) == type({})):
+            if (symbolTable[-1][x][USANDO] == NAO):
+                dictionarylist.append(x)
     symbolTable = symbolTable[0:-1]
+    return dictionarylist
 
 def addVar(name, type):
     global symbolTable
@@ -31,5 +66,6 @@ def getBindable(bindableName):
     global symbolTable
     for i in reversed(range(len(symbolTable))):
         if (bindableName in symbolTable[i].keys()):
+            symbolTable[i][bindableName][USANDO] = SIM
             return symbolTable[i][bindableName]
     return None
